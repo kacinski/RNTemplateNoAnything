689536b7106fba24607b3c931c46594ca8d06adb
slimecube98 <57504261+slimecube98@users.noreply.github.com>
2022-02-11 21:52:11 +0200

    upd

--- template/ios/Pods/Target Support Files/Pods-ProjectName-ProjectNameTests/Pods-ProjectName-ProjectNameTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProjectName_ProjectNameTests : NSObject
-@end
-@implementation PodsDummy_Pods_ProjectName_ProjectNameTests
-@end
--- template/ios/Pods/Target Support Files/Pods-ProjectName/Pods-ProjectName-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProjectName : NSObject
-@end
-@implementation PodsDummy_Pods_ProjectName
-@end
